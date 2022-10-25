e053fadc43d4c1f040b403fae75d585a232e025c
PCX-LK <62936489+PCX-LK@users.noreply.github.com>
2022-10-25 13:06:48 +0800

    Update mpconfigboard.h

--- LPK/boards/LPK/mpconfigboard.h
+++ LPK/boards/LPK/mpconfigboard.h
@@ -3,4 +3,4 @@
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (3 * 1024 * 1024)
 
 // Enable USB Mass Storage with FatFS filesystem.
- #define MICROPY_HW_USB_MSC  (1)
+#define MICROPY_HW_USB_MSC  (1)
